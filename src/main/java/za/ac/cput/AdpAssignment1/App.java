b98743697a8c06805205cff2991751b1e14a1b3f
Cameron van Wyk <219088470@mycput.ac.za>
2021-03-25 07:47:16 +0200

    Added variables

--- src/main/java/za/ac/cput/AdpAssignment1/App.java
+++ src/main/java/za/ac/cput/AdpAssignment1/App.java
@@ -6,6 +6,15 @@ package za.ac.cput.AdpAssignment1;
  */
 public class App 
 {
+    private String data;
+    private int number;
+
+    public App(String data, int number)
+    {
+        this.data = data;
+        this.number = number;
+    }
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
