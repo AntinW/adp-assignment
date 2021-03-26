6938743b3bb0533ce9a425077de76cd2ea2790ce
Tye <tyewalker09@gmail.com>
2021-03-26 15:03:12 +0200

    Added CarTest.java with an example of each test feature. Update junit to 5.7.0 in pom.xml for Timeout

---
+++ src/test/java/za/ac/cput/AdpAssignment1/CarTest.java
@@ -0,0 +1,72 @@
+package za.ac.cput.AdpAssignment1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+
+    private Car car1;
+    private Car car2;
+    private Car car3;
+
+    @BeforeEach
+    void setUp() {
+        car1 = new Car();
+        car2 = new Car();
+        car3 = car1;
+    }
+
+    /**
+     * i. Object Equality:
+     * This test checks if two values/objects are equal.
+     */
+    @Test
+    void testEquality() {
+        assertEquals(car1,car3);
+    }
+
+    /**
+     * ii. Object Identity:
+     * This test checks if two values/objects are the same.
+     */
+    @Test
+    void testIdentity() {
+        assertSame(car3, car1);
+    }
+
+    /**
+     * iii. Failing Test:
+     * This test is created to fail on purpose.
+     * This is done to make sure there are no false passes on other tests.
+     */
+    @Test
+    void testFailing()  {
+        fail("This failed.");
+    }
+
+    /**
+     * iv. Timeouts:
+     * This test will be stopped after 10 seconds.
+     */
+    @Test
+    @Timeout(10)
+    void testTimeout() throws InterruptedException {
+        Thread.sleep(5000);                         //Waits 5 seconds (5000 milliseconds)
+        System.out.println("This test passed!");
+    }
+
+    /**
+     * v. Disabling Test:
+     * This test is disabled by using "@Disabled".
+     */
+    @Disabled("This is an example of a disabled/ignored method")
+    @Test
+    void testDisabling() {
+        assertEquals(10+20, 15+15);
+    }
+
+}
