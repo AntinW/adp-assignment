58909d197cd74233ccaff285d9fedbd52156f50d
Tye <tyewalker09@gmail.com>
2021-03-29 14:54:53 +0200

    Created and added code to Car.java

---
+++ src/main/java/za/ac/cput/AdpAssignment1/Car.java
@@ -0,0 +1,56 @@
+package za.ac.cput.AdpAssignment1;
+
+/**
+ * @Author: Tye Walker - 218338562
+ * Car.java with Getters, Setters, and toString.
+ */
+
+public class Car {
+
+    private String make, model;
+    private int year;
+    private double price;
+
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                '}';
+    }
+}
