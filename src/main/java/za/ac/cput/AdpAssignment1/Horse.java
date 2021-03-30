b13d8592f3f14e701e9ba0e5d80f04507567c387
RLX-Wilkinson <81306123+RLX-Wilkinson@users.noreply.github.com>
2021-03-30 14:28:31 +0200

    Added Student Details

--- src/main/java/za/ac/cput/AdpAssignment1/Horse.java
+++ src/main/java/za/ac/cput/AdpAssignment1/Horse.java
@@ -1,5 +1,10 @@
 package za.ac.cput.AdpAssignment1;
 
+/**
+ * Author: Tauriq-Allie Wilkinsob
+ * Subject: ADP372S
+ * Student No.: 219071330
+ */
 public class Horse {
 
     private String name,color;
--- src/test/java/za/ac/cput/AdpAssignment1/HorseTest.java
+++ src/test/java/za/ac/cput/AdpAssignment1/HorseTest.java
@@ -1,5 +1,9 @@
 package za.ac.cput.AdpAssignment1;
-
+/**
+ * Author: Tauriq-Allie Wilkinsob
+ * Subject: ADP372S
+ * Student No.: 219071330
+ */
 public class HorseTest
 {
 
