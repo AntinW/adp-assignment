723018dd0a9cc250600c440b5695178e93ae88f7
Pints <abongiletshopi@gmail.com>
2021-03-31 09:34:00 +0200

    added fail method successfully and also added effective comments to all the methods in the class

--- src/test/java/za/ac/cput/AdpAssignment1/DVDTest.java
+++ src/test/java/za/ac/cput/AdpAssignment1/DVDTest.java
@@ -1,6 +1,7 @@
 package za.ac.cput.AdpAssignment1;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,15 +18,41 @@ class DVDTest {
          dvd2 =  dvd1;
     }
 
+    //Testing for equality of object DVD and found tha dvd1 == dvd2
+    //while dvd1 not == dvd0 , if i remove the comments in line num 26 the method fails
     @Test
     void testEquality() {
         assertEquals(dvd1, dvd2);
+        //assertEquals(dvd1, dvd0);
 
     }
 
+    //Testing for identity of DVD objects, dvd1 identical to dv2 while dvd1 and dvd0 || dvd2 and dvd0 not.
     @Test
-    void testIdentity(){
+    void testIdentity() {
         assertSame(dvd1, dvd2);
-
     }
+
+    // Testing the unit to fail unless you comment out line 40
+    @Test
+    void failTest(){
+        try{
+           fail("Test is failing");
+            } catch (Exception e) {
+            e.printStackTrace();
+            assertTrue(true);
+        }
+    }
+
+//    @Test (timeout = 1000)
+//    void testTimeout(){
+//        int maxNum = 2000;
+//
+//    }
+
+
+
+
+
+
 }
