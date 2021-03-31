ef76907f467e00c1ff03057863e510c1f6c24159
RLX-Wilkinson <81306123+RLX-Wilkinson@users.noreply.github.com>
2021-03-31 02:50:51 +0200

    Added Test Class

--- src/test/java/za/ac/cput/AdpAssignment1/HorseTest.java
+++ src/test/java/za/ac/cput/AdpAssignment1/HorseTest.java
@@ -1,10 +1,63 @@
 package za.ac.cput.AdpAssignment1;
-/**
- * Author: Tauriq-Allie Wilkinsob
- * Subject: ADP372S
- * Student No.: 219071330
- */
-public class HorseTest
-{
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HorseTest {
+
+    private Horse horse1,horse2,horse3;
+
+    @BeforeEach
+    void setUp()
+    {
+        horse1 = new Horse();
+        horse2 = new Horse();
+        horse3 = horse1;
+    }
+    /**
+     * i. Object Equality
+     */
+    @Test
+    void testEquality() {
+        assertEquals(horse1,horse3);
+    }
+
+    /**
+     * ii. Object Identity
+     */
+    @Test
+    void testIdentity() {
+        assertSame(horse3, horse1);
+    }
+
+    /**
+     * iii. Failing Test:
+     */
+    @Test
+    void testFailing()  { fail("Failed.");
+    }
+
+    /**
+     * iv. Timeouts
+     */
+    @Test
+    @Timeout(10)
+    void testTimeout() throws InterruptedException {
+        Thread.sleep(5000);                         //Waits 5 seconds (5000 milliseconds)
+        System.out.println(" Test Passed");
+    }
+
+    /**
+     * v. Disabling Test
+     */
+    @Disabled("This is an example of a disabled/ignored method")
+    @Test
+    void testDisabling() {
+        assertEquals(10+20, 15+15);
+    }
 
 }
