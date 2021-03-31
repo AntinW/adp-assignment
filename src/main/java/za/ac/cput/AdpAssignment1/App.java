011db81064e88a8a9c04050cd46c47c2cc3642a6
Robin White <robintylerw@gmail.com>
2021-03-31 15:29:48 +0200

    latest commit all code

--- src/main/java/za/ac/cput/AdpAssignment1/App.java
+++ src/main/java/za/ac/cput/AdpAssignment1/App.java
@@ -2,7 +2,7 @@ package za.ac.cput.AdpAssignment1;
 
 /**
  * Hello world!
- *
+ * author: Robin White - 218034555
  */
 public class App 
 {
--- src/test/java/za/ac/cput/AdpAssignment1/AppTest.java
+++ src/test/java/za/ac/cput/AdpAssignment1/AppTest.java
@@ -2,10 +2,16 @@ package za.ac.cput.AdpAssignment1;
 
 
 
-import org.junit.jupiter.api.Test;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.condition.DisabledIf;
+
 
 import static org.junit.jupiter.api.Assertions.*;
+
 /**
  *
  * @author : Robin White 218034555
@@ -21,22 +27,60 @@ public class AppTest
    private App app2;
    private App app3;
 
+
+
    @BeforeEach
     void setUp(){
        app1 = new App();
        app2 = new App();
        app3 = app1;
+
    }
 
+   //Equality Test
    @Test
    void testEquality(){
-       assertEquals(app1, app1);
+       assertEquals(app1, app3);
    }
 
+   //Identity Test
    @Test
     void testIdentity(){
-       assertSame(app1 , app3);
+       assertSame(app1 , app1);
    }
+
+   //Failing Test
+   @Test
+     void testFail(){
+       if(app1 != app2){
+           fail("This Test has failed");
+       }
+   }
+
+   //Disable Test
+    @Disabled("Test was DISABLED")
+    @Test
+     void testDisable(){
+       assertEquals(10, 9+1);
+   }
+
+    //Timeout Test
+    @Test
+    @Timeout(3)
+    void testTimeout() throws InterruptedException{
+        Thread.sleep(5000);
+        while(true) {
+        }
+    }
+
+
+
+
+
+
+
+
+
 
 
 
